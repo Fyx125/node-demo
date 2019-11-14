e715f68e2f25c42418eda1b25674b053320211f0
yx666 <1251973330@qq.com>
2019-11-14 17:54:51 +0800

    加了一个数组接口

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ const router=require('koa-router');
 let server=new koa();
 server.listen(8081);
 
+server.use(async (ctx,next)=>{
+    ctx.set('Access-Control-Allow-Origin', '*');
+    await next();
+});
+
 let r1=new router();
 server.use(r1.routes());
 
@@ -27,3 +32,14 @@ r1.get('/api/page/:page',async ctx=>{
        ctx.response.body={err:false,data:data};
    }
 });
+
+r1.get('/list',async ctx=>{
+   ctx.body=[
+       {id: '1', name: '玩具', price: 555, sales: 5555},
+       {id: '2', name: '衣服', price: 666, sales: 362},
+       {id: '3', name: '鞋子', price: 777, sales: 231},
+       {id: '4', name: '手表', price: 897, sales: 987}
+   ];
+});
+
+
